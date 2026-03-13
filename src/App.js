c977dea1e68ba17593a5cf42517a01f6d0517e67
Mxles-Arm <ccooppto@gmail.com>
2026-03-13 19:49:33 +0700

    updata

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import Home from "./components/home";
 import Edit from "./components/Edit";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./components/app.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="container text-center mt-4">
         <h1 className="oasis-title">Student Phone</h1>
 
         <div className="oasis-card">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/std-phone" element={<Home />} />
             <Route path="/edit" element={<Edit />} />
           </Routes>
         </div>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
